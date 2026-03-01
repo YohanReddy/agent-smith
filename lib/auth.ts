5774c2852d3ec383928cae6d978fea1195acabd4
Yella Yohan Reddy <66876592+YohanReddy@users.noreply.github.com>
2026-03-01 23:47:41 +0530

    add: better-auth

---
+++ lib/auth.ts
@@ -0,0 +1,9 @@
+import { betterAuth } from "better-auth";
+import { dash } from "@better-auth/infra";
+
+export const auth = betterAuth({
+  baseURL: "http://localhost:3000/",
+  plugins: [
+    dash()
+  ]
+});
